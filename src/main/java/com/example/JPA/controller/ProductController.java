5002e5b89c03995b3fbea11f846de9fa9e3b8a5c
Augusto <augustooliveixa@gmail.com>
2023-09-26 01:26:01 -0300

    Adding HTTP methods for querying the database and commenting on the Statistical List

--- src/main/java/com/example/JPA/controller/ProductController.java
+++ src/main/java/com/example/JPA/controller/ProductController.java
@@ -1,46 +1,63 @@
 package com.example.JPA.controller;
 
+import com.example.JPA.ProductRepository;
 import com.example.JPA.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.util.ArrayList;
+//import java.util.ArrayList;
 import java.util.List;
 
 
 @RestController // controller de arquuitetura web RESTful
-@RequestMapping("/products")
+@RequestMapping("/produtos")
 public class ProductController {
-    private final List<Product> products = new ArrayList<>(); // modificador de java "final" é aplicado para determiinar uma constante
 
-    public ProductController(){
-        products.add(new Product(1L, "Amaciante", 10.0));
-        products.add(new Product(2L, "Sabão me pó", 8.90));
-        products.add(new Product(3L, "Fogão", 900.0));
-        products.add(new Product(4L, "Sofa", 950.0));
-    }
+//    private final List<Product> products = new ArrayList<>(); // modificador de java "final" é aplicado para determiinar uma constante
+
+//    public ProductController(){
+//        products.add(new Product(1L, "Amaciante", 10.0));
+//        products.add(new Product(2L, "Sabão me pó", 8.90));
+//        products.add(new Product(3L, "Fogão", 900.0));
+//        products.add(new Product(4L, "Sofa", 950.0));
+//    }
+
+
+    @Autowired // auxilia a conexao com a interface ProductRepository
+    private ProductRepository productRepository; // injetando uma instancia e tendo acesso a base de dados definida de interface
 
     @GetMapping
     public List<Product> getAllProducts(){
-        return products;
+        return productRepository.findAll(); // retorna todos os produtoes da tabela Product
     }
 
     @GetMapping("/{id}")
     public Product getProductById(@PathVariable Long id){
-        return null;
+        return productRepository.findById(id).orElse(null); // buscando produto na base de dados, caso nao tenha valor retorna nulo
     }
 
     @PostMapping
     public Product createProduct(@RequestBody Product product){
-        return null;
+        return productRepository.save(product); // criando objeto e instanciado na base de dados
     }
 
-    @PutMapping
+    @PutMapping("/{id}")
     public Product updateProduct(@PathVariable Long id, @RequestBody Product product){
+        if(productRepository.existsById(id)){
+            product.setId(id);
+            return productRepository.save(product);
+        }
+
         return null;
+//        return (Product) ResponseEntity.notFound();
     }
 
-    @DeleteMapping
+    @DeleteMapping("/{id}")
     public  Product deleteProdut(@PathVariable long id){
-        return null;
+        Product product = productRepository.findById(id).orElse(null);
+        if(product != null){
+            productRepository.delete(product);
+        }
+        return product;
     }
-
 }
